c030eb9b6a6aa9f9dec23d5cb2d07e8378711cb6
pasindubandara <pasindu.bandara@parentpay.com>
2026-09-12 09:59:08 +0530

    run analysis update

--- run_analysis.py
+++ run_analysis.py
@@ -7,19 +7,18 @@ from ensemble_predict import ensemble_evaluate
 from knowledge_distillation import knowledge_distillation
 from pruning import pruning
 from quantization import quantization
-from predict_severity import predict_severity
 
 reports_dir   = r"C:\Private\Private proj\DiabeticRetinopathy\notebooks\reports"
 csv_path      = os.path.join(reports_dir, "dataset_analysis.csv")
 os.makedirs(reports_dir, exist_ok=True)
-raw_dir = r"C:\Private\Private proj\DiabeticRetinopathy\raw 20 from each sev"
+raw_dir = r"C:\Private\Private proj\DiabeticRetinopathy\raw 10 from each sev"
 processed_dir  = r"C:\Private\Private proj\DiabeticRetinopathy\processed"
 prediction_dir = r"C:\Private\Private proj\DiabeticRetinopathy\notebooks\prediction images"
 
 # analyze_dataset (    
 #     output_csv=csv_path,
 #     raw_dir=raw_dir,
-#     target_size=(600, 600),
+#     target_size=(224, 224),
 #     blur_threshold=5,
 #     contrast_threshold=40.0,
 #     illumination_threshold=15.0,
@@ -29,71 +28,61 @@ prediction_dir = r"C:\Private\Private proj\DiabeticRetinopathy\notebooks\predict
 #     input_csv = csv_path,
 #     raw_dir=raw_dir,
 #     processed_dir=processed_dir,
-#     target_size=(600, 600),
-#     blur_threshold=5,
+#     target_size=(224, 224)
 # )
 
-# mobileNet_data_modeling(
-#     processed_dir=processed_dir,
-#     num_classes=5,
-#     epochs=20,
-#     batch_size=32,
-#     learning_rate=1e-4,
-#     img_size=224,
-# )
-
-# efficientnet_lite_data_modeling(
-#     processed_dir=processed_dir,
-#     num_classes=5,
-#     epochs=20,
-#     batch_size=32,
-#     learning_rate=1e-4,
-#     img_size=224,
-# )
-
-# ensemble_evaluate(
-#     processed_dir=processed_dir,
-#     num_classes=5,
-#     batch_size=32,
-#     img_size=224,
-#     mobilenet_weight=0.5,
-#     efficientnet_weight=0.7,
-# )
-
-# knowledge_distillation(
-#     processed_dir=processed_dir,
-#     num_classes=5,
-#     epochs=20,
-#     batch_size=32,
-#     learning_rate=1e-4,
-#     img_size=224,
-#     temperature=4.0,
-#     alpha=0.7,
-#     mobilenet_weight=0.5,
-#     efficientnet_weight=0.7,
-# )
-
-# pruning(
-#     processed_dir=processed_dir,
-#     num_classes=5,
-#     epochs=15,
-#     batch_size=32,
-#     learning_rate=1e-4,
-#     img_size=224,
-#     pruning_amount=0.2,
-# )
-
-# quantization(
-#     processed_dir=processed_dir,
-#     num_classes=5,
-#     batch_size=32,
-#     img_size=224,
-# )
-
-predict_severity(
-	processed_dir=processed_dir,
-	prediction_dir=prediction_dir,
-	num_classes=5,
-	img_size=224,
-	max_images=4,
+mobileNet_data_modeling(
+    processed_dir=processed_dir,
+    num_classes=5,
+    epochs=20,
+    batch_size=32,
+    learning_rate=1e-4,
 )
+
+efficientnet_lite_data_modeling(
+    processed_dir=processed_dir,
+    num_classes=5,
+    epochs=20,
+    batch_size=32,
+    learning_rate=1e-4,
+)
+
+ensemble_evaluate(
+    processed_dir=processed_dir,
+    num_classes=5,
+    batch_size=32,
+    img_size=224,
+    mobilenet_weight=0.5,
+    efficientnet_weight=0.7,
+)
+
+knowledge_distillation(
+    processed_dir=processed_dir,
+    num_classes=5,
+    epochs=20,
+    batch_size=32,
+    learning_rate=1e-4,
+    img_size=224,
+    temperature=4.0,
+    alpha=0.7,
+    mobilenet_weight=0.5,
+    efficientnet_weight=0.7,
+)
+
+pruning(
+    processed_dir=processed_dir,
+    num_classes=5,
+    epochs=15,
+    batch_size=32,
+    learning_rate=1e-4,
+    img_size=224,
+    pruning_amount=0.2,
+)
+
+quantization(
+    processed_dir=processed_dir,
+    num_classes=5,
+    batch_size=32,
+)
+
+
